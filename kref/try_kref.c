32d6bf34cab75210058607c125c74b06cc096ee4
TangXC <tangxch@foxmail.com>
2016-07-10 19:58:42 +0800

    add kref module
    Signed-off-by: TangXC <tangxch@foxmail.com>

---
+++ kref/try_kref.c
@@ -0,0 +1,66 @@
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/slab.h>
+#include <linux/kthread.h>
+
+MODULE_LICENSE("GPL");
+struct my_data
+{
+    struct kref refcount;
+    int num;
+};
+
+void data_release(struct kref *ref)
+{
+    struct my_data * data=container_of(ref,struct my_data, refcount);
+    kfree(data);
+}
+
+void more_data_handling(void *cb_data)
+{
+    struct my_data *data=cb_data;
+    data->num++;
+    printk(KERN_EMERG "End of more_data_handling %d\n", data->num);
+    kref_put(&data->refcount,data_release);
+}
+
+int my_data_handler(void)
+{
+    int rv=0;
+    struct my_data *data;
+    struct task_struct *task;
+    data=kmalloc(sizeof(*data), GFP_KERNEL);
+    if (!data)
+        return -ENOMEM;
+    data->num=0;
+    kref_init(&data->refcount);
+    kref_get(&data->refcount);
+    task=kthread_run(more_data_handling, data, "more_data_handling");
+    if(task==ERR_PTR(-ENOMEM))
+    {
+        rv=-ENOMEM;
+        goto out;
+    }
+    data->num++;
+ out:
+    kref_put(&data->refcount, data_release);
+    printk(KERN_EMERG "End of my_data_handler %d\n", data->num);
+    return rv;
+}
+
+static int __init try_kref_init(void)
+{
+	printk(KERN_EMERG "try_kref_init\n");
+        my_data_handler();
+	return 0;
+}
+
+static void __exit try_kref_exit(void)
+{
+	printk(KERN_EMERG "try_kref_exit\n");
+	return;
+}
+
+module_init(try_kref_init);
+module_exit(try_kref_exit);
