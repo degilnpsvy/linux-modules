5cab182ed5223daa7cc59921f83356ab2c7df025
TangXC <tangxch@foxmail.com>
2016-07-10 15:09:23 +0800

    use create_singlethread_workqueue and flush_workqueue
    Signed-off-by: TangXC <tangxch@foxmail.com>

--- workqueue/try_workqueue.c
+++ workqueue/try_workqueue.c
@@ -5,17 +5,26 @@
 
 MODULE_LICENSE("GPL");
 
+static struct workqueue_struct *wq;
+static int a=0;
+
 static void work_handler(struct work_struct *data)
 {
-    printk(KERN_ALERT "work handler function\n");
+    printk(KERN_ALERT "work handler function%d\n",a++);
 }
 static struct work_struct work;
 
 static int __init try_workqueue_init(void)
 {
 	printk(KERN_EMERG "try_workqueue_init\n");
+        wq=create_singlethread_workqueue("my_singlethread_workqueue");
         INIT_WORK(&work,work_handler);
-        schedule_work(&work);
+        queue_work(wq,&work);
+        queue_work(wq,&work);
+        printk(KERN_EMERG "try_workqueue flush before\n");
+        flush_workqueue(wq);
+        printk(KERN_EMERG "try_workqueue flush after\n");
+        queue_work(wq,&work);
 	return 0;
 }
 
